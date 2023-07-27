24ea77ce923119cedf0231dc3fa071364f3a7741
sunilkumarnreddy <sunilkumarn55555@gmail.com>
2023-07-27 15:34:04 +0530

    newbranch

--- app/views.py
+++ app/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 from django.core.mail import send_mail
 from app.forms import *
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
 
 
 def registration(request):
     usfo=UserForm()
     pfo=ProfileForm()
     d={'usfo':usfo,'pfo':pfo}
+
     if request.method=='POST' and request.FILES:
         usfd=UserForm(request.POST)
         pfd=ProfileForm(request.POST,request.FILES)
@@ -18,14 +23,54 @@ def registration(request):
             submittedPW=usfd.cleaned_data['password']
             NSUFO.set_password(submittedPW)
             NSUFO.save()
+
             NSPO=pfd.save(commit=False)
             NSPO.username=NSUFO
             NSPO.save()
+
             send_mail('Registration',
-                      'Ur Registration is Successfull',
+                    'Ur Registration is Successfull',
                       'shaikharshadvali79@gmail.com',
                       [NSUFO.email],
                       fail_silently=False
-                      )
+                          )
+
+
+
             return HttpResponse('Registration is Succeffully check in admin')
+
+
+
     return render(request,'registration.html',d)
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+
+    return render(request,'home.html')
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        AUO=authenticate(username=username,password=password)
+        if AUO:
+            if AUO.is_active:
+                login(request,AUO)
+                request.session['username']=username
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                return HttpResponse('Not a Active User')
+        else:
+            return HttpResponse('Invalid Details')
+    return render(request,'user_login.html')
+
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
--- p22_usermodels/urls.py
+++ p22_usermodels/urls.py
@@ -1,7 +1,8 @@
-"""p22_usermodels URL Configuration
+"""
+URL configuration for project23 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -20,6 +21,11 @@ from django.conf.urls.static import static
 from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('registration/',registration,name='registration'),
+    path('registration/',registration,name='registration'),
+    path('home/',home,name='home'),
+    path('user_login/',user_login,name='user_login'),
+    path('user_logout/',user_logout,name='user_logout'),
+
+
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
